b14eaec4bbdb462a4181a1a161d05cb1b9f7abde
Sanjana Kashyap <100091218+sanjanakash@users.noreply.github.com>
2023-03-28 17:28:59 -0400

    navigation bar skeleton is functional

---
+++ app/src/main/java/com/example/cs5520finalproject/FragmentQuestHomePage.java
@@ -0,0 +1,29 @@
+package com.example.cs5520finalproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentQuestHomePage extends Fragment {
+
+    public FragmentQuestHomePage() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quest_home_page, container, false);
+    }
+}
