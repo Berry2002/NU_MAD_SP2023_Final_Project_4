24191039cb77b570eddaa0a2df84fd7054065276
Yimeng Chen <yimeng2002@icloud.com>
2023-03-29 15:46:26 -0400

    Search page front end skeleton

--- app/src/main/java/com/example/cs5520finalproject/FragmentSearchPage.java
+++ app/src/main/java/com/example/cs5520finalproject/FragmentSearchPage.java
@@ -58,7 +58,7 @@ public class FragmentSearchPage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_search_page, container, false);
+        return view;
     }
 }
