420b2b04c81c4c862ca1c9b9b685702154229ec6
Yimeng Chen <yimeng2002@icloud.com>
2023-04-09 15:32:41 -0400

    searchPath, rankings, and equipPath pages & adapters

--- app/src/main/java/com/example/cs5520finalproject/FragmentEquipPathPage.java
+++ app/src/main/java/com/example/cs5520finalproject/FragmentEquipPathPage.java
@@ -1,64 +1,114 @@
 package com.example.cs5520finalproject;
 
+import static com.example.cs5520finalproject.Tags.EQUIPPATHPAGE;
+
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentEquipPathPage#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class FragmentEquipPathPage extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TextView textViewEquipPathName, textViewEquipPathDescription;
+    private Button buttonEquipPath;
+    private ImageView imageViewEquipPath;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Path selectedPath;
+
+    private FirebaseFirestore db;
+
+    private IEquipPath mListener;
 
     public FragmentEquipPathPage() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentEquipPathPage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentEquipPathPage newInstance(String param1, String param2) {
-        FragmentEquipPathPage fragment = new FragmentEquipPathPage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public FragmentEquipPathPage(Path path) {
+        this.selectedPath = path;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            db = FirebaseFirestore.getInstance();
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_equip_path_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_equip_path_page, container, false);
+
+        textViewEquipPathName = view.findViewById(R.id.textViewEquipPathName);
+        textViewEquipPathDescription = view.findViewById(R.id.textViewEquipPathDescription);
+        buttonEquipPath = view.findViewById(R.id.buttonEquipPath);
+        imageViewEquipPath = view.findViewById(R.id.imageViewEquipPath);
+
+        fetchCurrentPath(selectedPath);
+
+        // jump to home page displaying list of quests contained in this path
+        imageViewEquipPath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.equipPath(selectedPath);
+            }
+        });
+
+        return view;
+    }
+
+    private void fetchCurrentPath(Path path) {
+        DocumentReference mPath = db.collection("paths")
+                .document(path.getLocation()); // get document path location??
+
+        // get path document
+        mPath.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                        // set corresponding path info
+                        textViewEquipPathName.setText(
+                                new StringBuilder(selectedPath.getLocation()
+                                        + " - "
+                                        + selectedPath.getSubject()));
+                        textViewEquipPathDescription.setText(selectedPath.getDescription());
+                        imageViewEquipPath.setImageResource(Integer.parseInt(selectedPath.getImage()));//imageurl???
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(EQUIPPATHPAGE, "fetchPathInfo failed");
+                    }
+                });
+    }
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof IEquipPath){
+            mListener = (IEquipPath) context;
+        } else{
+            throw new RuntimeException(context.toString()+ "must implement IEquipPath");
+        }
+    }
+
+    public interface IEquipPath {
+        void equipPath(Path path);
     }
 }
--- app/src/main/java/com/example/cs5520finalproject/PathsAdapter.java
+++ app/src/main/java/com/example/cs5520finalproject/PathsAdapter.java
@@ -1,2 +1,85 @@
-package com.example.cs5520finalproject;public class PathsAdapter {
+package com.example.cs5520finalproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class PathsAdapter extends RecyclerView.Adapter<PathsAdapter.ViewHolder> {
+
+    private IPaths mListener;
+
+    ArrayList<Path> mPaths;
+
+    public PathsAdapter() {
+
+    }
+
+    public PathsAdapter(ArrayList<Path> mPaths, Context context) {
+        this.mPaths = mPaths;
+        if(context instanceof IPaths){
+            this.mListener = (IPaths) context;
+        } else{
+            throw new RuntimeException(context.toString()+ "must implement IPaths");
+        }
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageViewPath;
+        private TextView textViewPathLocation, textViewSubject, textViewPathDescription;
+        private Button buttonSeePathHighlights;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imageViewPath = itemView.findViewById(R.id.imageViewPath);
+            this.textViewPathLocation = itemView.findViewById(R.id.textViewPathLocation);
+            this.textViewSubject = itemView.findViewById(R.id.textViewSubject);
+            this.textViewPathDescription = itemView.findViewById(R.id.textViewPathDescription);
+            this.buttonSeePathHighlights = itemView.findViewById(R.id.buttonSeePathHighlights);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemRecyclerView = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.path_card,parent, false);
+
+        return new ViewHolder(itemRecyclerView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PathsAdapter.ViewHolder holder, int position) {
+        Path currentPath = this.mPaths.get(position);
+
+        holder.textViewPathDescription.setText(currentPath.getDescription());
+        holder.textViewSubject.setText(currentPath.getSubject());
+        holder.imageViewPath.setImageResource(Integer.parseInt(currentPath.getImage()));
+        holder.textViewPathLocation.setText(currentPath.getLocation());
+        holder.buttonSeePathHighlights.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.goToPathHighlights(mPaths.get(holder.getAdapterPosition()));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.mPaths.size();
+    }
+    public interface IPaths {
+        void goToPathHighlights(Path path);
+    }
 }
+
+
