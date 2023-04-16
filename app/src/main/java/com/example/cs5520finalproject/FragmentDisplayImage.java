f2e79d36a81876241b82d674d5ba7b2f4f405cc8
Yimeng Chen <yimeng2002@icloud.com>
2023-04-16 13:16:21 -0400

    FIXME: 2 camera fragments running parallel

--- app/src/main/java/com/example/cs5520finalproject/FragmentDisplayImage.java
+++ app/src/main/java/com/example/cs5520finalproject/FragmentDisplayImage.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -53,6 +54,7 @@ public class FragmentDisplayImage extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             imageUri = getArguments().getParcelable(ARG_URI);
+            Log.d("display image", "onCreate");
         }
     }
 
